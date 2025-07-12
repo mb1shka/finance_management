88178cda4d2aa3f03cd6136742e66715a79d0373
mb1shka1 <ekaterina.mishakova.a@gmail.com>
2025-07-12 23:39:42 +0300

    Compain sliverappbar elements position

--- lib/screens/home/view/home_sliver_app_bar.dart
+++ lib/screens/home/view/home_sliver_app_bar.dart
@@ -2,6 +2,9 @@ import 'package:finance_management/screens/home/view/greeting_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:finance_management/design_system/design_system.dart';
 
+const double _kPadding = 36;
+const double _kRoundedBorderHeight = 30;
+
 class HomeSliverAppBar extends StatelessWidget {
   const HomeSliverAppBar({super.key});
 
@@ -9,40 +12,39 @@ class HomeSliverAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverAppBar(
       expandedHeight: 220,
-      collapsedHeight: 100,
+      collapsedHeight: 150,
       pinned: true,
       backgroundColor: AppColors.caribbeanGreen,
       elevation: 0,
-      flexibleSpace: DecoratedBox(
-        decoration: BoxDecoration(
-          color: AppColors.caribbeanGreen,
-        ),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                height: 30,
-                decoration: BoxDecoration(
-                  color: AppColors.honeyDrew,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(AppRadiuses.xxxl)),
+      flexibleSpace: LayoutBuilder(
+        builder: (context, constraints) {
+          final double greetingOpacity =
+              (constraints.maxHeight - 150) / (220 - 150); // 200 = expandedHeight, 100 = collapsedHeight
+          return DecoratedBox(
+            decoration: BoxDecoration(color: AppColors.caribbeanGreen),
+            child: Stack(
+              children: [
+                Positioned(bottom: _kRoundedBorderHeight + _kPadding, child: Container(height: 30, width: 40, color: Colors.lightBlue)),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: _kRoundedBorderHeight,
+                    decoration: BoxDecoration(
+                      color: AppColors.honeyDrew,
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(AppRadiuses.xxxl)),
+                    ),
+                  ),
                 ),
-              ),
+                Positioned(
+                  top: _kPadding,
+                  left: _kPadding,
+                  child: Opacity(opacity: greetingOpacity.clamp(0.0, 1.0), child: GreetingWidget()),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(36, 36, 36, 0),
-              child: Column(
-                children: [
-                  GreetingWidget(),
-                ],
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
-
-
 }
-
