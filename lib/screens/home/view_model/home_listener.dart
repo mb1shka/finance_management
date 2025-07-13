2d0d2a898d9e611644bc74a11da32d4e5e8c78d8
mb1shka1 <ekaterina.mishakova.a@gmail.com>
2025-07-14 01:24:33 +0300

    Fixup

--- lib/screens/home/view_model/home_listener.dart
+++ lib/screens/home/view_model/home_listener.dart
@@ -5,11 +5,4 @@ import 'package:finance_management/di/di.dart';
 class HomeListener extends ChangeNotifier {
   final BudgetRepository budgetRepository = di();
 
-  // try {
-  // await budgetRepository.createBudgetPlan(myPlan);
-  // } on BudgetValidationException catch (e) {
-  // showError('Некорректные данные: ${e.message}');
-  // } on BudgetStorageException catch (e) {
-  // showError('Ошибка сохранения: ${e.message}');
-  // }
 }
